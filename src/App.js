c98ab1c7caa5ade2794fcdc560b05bd18bdc48c2
gillyhuga <gilly.huga@yahoo.co.id>
2021-04-27 22:40:36 +0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 // import axios from 'axios'
 
 // const api = axios.create({
@@ -14,21 +15,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button>Test</Button>
     </div>
   );
 }
---
+++ src/Pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainPage = () =>{
+
+    return (
+        <div>
+            <h3>Index</h3>
+            <small>main page</small>
+        </div>
+    );
+}
+
+export default MainPage; 
