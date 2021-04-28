9317326aba01bdcd74856ab6d90bff033a54643b
gillyhuga <gilly.huga@yahoo.co.id>
2021-04-28 23:53:51 +0700

    add footer

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../Components/Navbar/navbar';
+import Footer from '../Components/Footer/footer';
 
 const MainPage = () =>{
 
@@ -8,6 +9,7 @@ const MainPage = () =>{
             <Navbar />
             {/* <h3>Index</h3>
             <small>mainssssssssssscccss page</small> */}
+            <Footer/>
         </div>
     );
 }
