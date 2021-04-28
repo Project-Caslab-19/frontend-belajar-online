509a4872887d30f9c96086e4502e414a35f13b0e
Raldienpr325 <aldinprayoga26@webmail.umm.ac.id>
2021-04-29 01:05:30 +0700

    udahhhhh

--- src/Components/Section1/Section1.js
+++ src/Components/Section1/Section1.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import './Section1.css'
 import { ReactComponent as Section1img } from './section1.svg';
 
 
 function Section1() {
     return (
         <div className="section1">
-
-            <Container>
+            <Section1img className="gambarsection1" />
+            <Container className="ctn-section1">
                 <Row className="justify-content-md-center">
-                    <Section1img className="gambarsection1" />
                     <Col md="auto"> <h1 className="judulsection1">Kenapa Lab Academy ?</h1></Col>
                     <Col md="auto"> <h2 className="judulsection1">Saatnya bijak memilih sumber belajar. Tak hanya materi yang terjamin,Lab Academy juga memiliki pemateri profesional yang akan meberikan ilmu anda.</h2></Col>
                 </Row>
@@ -21,5 +21,4 @@ function Section1() {
         </div>
     )
 }
-
 export default Section1
