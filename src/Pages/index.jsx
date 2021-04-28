490d343bdbb9dcf9fa0cfc5cf10055e7f4c9da90
qiqinn <qiqinn0000@gmail.com>
2021-04-29 00:51:22 +0800

    add heroku

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Navbar from '../Components/Navbar/navbar';
+import Hero from '../Components/Hero/landingpage';
 
 const MainPage = () =>{
 
     return (
         <div>
             <Navbar />
+            <Hero />
             {/* <h3>Index</h3>
             <small>mainssssssssssscccss page</small> */}
         </div>
+        
     );
 }
 
