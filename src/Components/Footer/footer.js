1b0f278c4e4b66b2e9e6d72ada91d7f1b7ced117
gillyhuga <gilly.huga@yahoo.co.id>
2021-04-29 20:09:34 +0700

    add footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,19 +1,45 @@
 import React from "react";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 
 const Footer = () => {
   return (
-    <div class="d-flex flex-column">
-  <footer class="footer">
-    <div>
-      <a href="">Lab Academy</a>
-      <span>&copy; 2021 Lab Academy.</span>
-    </div>
-    <div class="ml-auto">
-      <span>Powered by</span>
-      <a href="">Lab Academy</a>
-    </div>
-  </footer>
-</div>
+    <Container className="ctn-hero">
+  <Row>
+    <Col sm={10}>
+    <Row>
+      <h4>
+        Lab Academy
+      </h4>
+    </Row>
+      </Col>
+    <Col sm={2}>
+    <Row>
+    <Col>
+    <a href="" className="a">
+    <FontAwesomeIcon icon={faInstagram} />
+    </a>
+    </Col>
+    <Col>
+    <a href="" className="a">
+    <FontAwesomeIcon icon={faTwitter} />
+    </a>
+    </Col>
+    <Col>
+    <a href="https://github.com/Project-Caslab-19" className="a">
+    <FontAwesomeIcon icon={faGithub} />
+    </a>
+    </Col>
+    </Row>
+    </Col>
+  </Row>
+</Container>
   );
 }
 
