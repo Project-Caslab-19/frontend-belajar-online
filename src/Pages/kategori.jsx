501e12814e40ea33694921e7079686063d8bd597
Raldienpr325 <aldinprayoga26@webmail.umm.ac.id>
2021-05-08 20:44:11 +0700

    aqin

--- src/Pages/kategori.jsx
+++ src/Pages/kategori.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import NavigationBar from '../Components/Navbar/navbar';
 
 import Footer from '../Components/Footer/footer';
-
+import Sidebar from '../Components/Sidebar/sidebar'
 import KategoriKelas from '../Components/KategoriKelas/KategoriKelas'
 
 const UserPage_kategori = () => {
 
     return (
         <div>
+
             <NavigationBar />
             <KategoriKelas />
             <Footer />
