bc47610d427da485a8259aff52ded3e1109d74cf
Aldinnn <79825157+Raldienpr325@users.noreply.github.com>
2021-05-08 20:50:16 +0700

    Revert "style button"

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import NavigationBar from '../Components/Navbar/navbar';
 import Section1 from '../Components/Section1/Section1';
 import Section2 from '../Components/Section2/Section2';
 import Footer from '../Components/Footer/footer';
 import Hero from '../Components/Hero/landingpage';
-import Navbar from '../Components/Navbar/navbar'
 
 
 const MainPage = () => {
 
     return (
         <div>
-            <Navbar/>
+            <NavigationBar />
             <Hero />
             <Section1 />
             <Section2 />
