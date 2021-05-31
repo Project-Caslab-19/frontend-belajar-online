1fabba2b3b45632f319a747827ff3aa5b3cd5e20
gillyhuga <gilly.huga@yahoo.co.id>
2021-05-31 13:04:35 +0700

    add : daftar kelas page

---
+++ src/Components/DaftarKelas/daftarKelas.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserAlt } from '@fortawesome/free-solid-svg-icons'
+import { faBook } from '@fortawesome/free-solid-svg-icons'
+
+
+function Daftarkelas() {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col sm={8} style={{ fontSize: 24, fontWeight: 'bold' }}>Daftar Kelas</Col>
+                </Row>
+                <Row style={{ marginTop: 20 }}>
+
+                    <button className={"buttonMedium"} >
+                        Tambah Kelas
+                </button>
+                </Row>
+                <Row style={{ marginTop: 20 }} className="justify-content-center">
+                    <Col sm>
+                        <Card >
+                            <Card.Header className="bgCardDashoard" style={{ fontWeight: 'bold', color: "white" }}>Membuat Database</Card.Header>
+                            <Card.Body>
+                                <Card.Title>
+                                    <Row >
+                                        <Col>
+                                            <Row >
+                                                <Col xs lg="2">
+                                                    <FontAwesomeIcon icon={faUserAlt} />
+                                                </Col>
+                                                <Col >
+                                                    <p>67</p>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                        <Col>
+                                            <Row >
+                                                <Col xs lg="2">
+                                                    <FontAwesomeIcon icon={faBook} />
+                                                </Col>
+                                                <Col >
+                                                    <p>666</p>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                </Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm>
+                        <Card >
+                            <Card.Header className="bgCardDashoard" style={{ fontWeight: 'bold', color: "white" }}>Membuat Database</Card.Header>
+                            <Card.Body>
+                                <Card.Title>
+                                    <Row >
+                                        <Col>
+                                            <Row >
+                                                <Col xs lg="2">
+                                                    <FontAwesomeIcon icon={faUserAlt} />
+                                                </Col>
+                                                <Col >
+                                                    <p>67</p>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                        <Col>
+                                            <Row >
+                                                <Col xs lg="2">
+                                                    <FontAwesomeIcon icon={faBook} />
+                                                </Col>
+                                                <Col >
+                                                    <p>666</p>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                </Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm>
+                        <Card >
+                            <Card.Header className="bgCardDashoard" style={{ fontWeight: 'bold', color: "white" }}>Membuat Database</Card.Header>
+                            <Card.Body>
+                                <Card.Title>
+                                    <Row >
+                                        <Col>
+                                            <Row >
+                                                <Col xs lg="2">
+                                                    <FontAwesomeIcon icon={faUserAlt} />
+                                                </Col>
+                                                <Col >
+                                                    <p>67</p>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                        <Col>
+                                            <Row >
+                                                <Col xs lg="2">
+                                                    <FontAwesomeIcon icon={faBook} />
+                                                </Col>
+                                                <Col >
+                                                    <p>666</p>
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                </Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+
+}
+export default Daftarkelas
