a7a63b5ba55e9704defe71efb68381db837142d2
gillyhuga <gilly.huga@yahoo.co.id>
2021-06-11 20:21:42 +0700

    edit navbar

--- src/Pages/edit-materi-kelas.jsx
+++ src/Pages/edit-materi-kelas.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import Sidebar3 from '../Components/sidebar3/sidebar3';
 import EditMateri from '../Components/EditMateriKelasAdmin/editmaterikelas'
-import Navbar3 from '../Components/Navbar3/navbar3';
+import Navbar from '../Components/Navbar2/navbar2';
 
 const AdminPage_editmateri = () => {
 
     return (
         <>
             <div>
-                <Navbar3 />
+                <Navbar />
                 <Container fluid>
                     <Row>
                         <Col xs={2} id="sidebar-wrapper">
--- src/Pages/tambah-quiz-admin.jsx
+++ src/Pages/tambah-quiz-admin.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import Sidebar3 from '../Components/sidebar3/sidebar3';
 import TambahQuiz from '../Components/TambahQuiz/tambahquiz'
-import Navbar3 from '../Components/Navbar3/navbar3';
+import Navbar from '../Components/Navbar2/navbar2';
 
 const AdminPage_TambahQuiz = () => {
 
     return (
         <>
             <div>
-                <Navbar3 />
+                <Navbar />
                 <Container fluid>
                     <Row>
                         <Col xs={2} id="sidebar-wrapper">
